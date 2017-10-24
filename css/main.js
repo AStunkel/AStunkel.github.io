75c91d4643f896b51f454e6d7f51fb1c4e148101
AStunkel <atpoertner@gmail.com>
2017-10-24 09:22:10 -0400

    Started Javascipt

--- css/main.js
+++ css/main.js
@@ -1,9 +1,1 @@
->var totalTrains=12;
->var trainsOperational=8;
-console.log("There are" + trainsOperational + "running trains.");
 
-var trainNumber=1'
-while(trainNumber <=8){
-  console.log("Train #" + trainNumber + "is running.");
-  trainNumber + +;
-}
